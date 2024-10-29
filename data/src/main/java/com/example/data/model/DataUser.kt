62f18f60120bbf78547470b4bbf8201eb14bf61b
yunsuk990 <yunsuk990@naver.com>
2024-10-29 21:33:22 +0900

    Domain, Data 계층 분리

---
+++ data/src/main/java/com/example/data/model/DataUser.kt
@@ -0,0 +1,6 @@
+package com.example.data.model
+
+data class DataUser(
+    val name: String,
+    val age: Int
+)
---
+++ domain/src/main/java/com/example/domain/model/DomainUser.kt
@@ -0,0 +1,7 @@
+package com.example.domain.model
+
+data class DomainUser(
+    val uid: String,
+    val name: String,
+    val age: Int
+)
