c3a7c710400d906b82bed17e552c08b9a9459a74
yunsuk990 <yunsuk990@naver.com>
2024-11-13 21:56:31 +0900

    Drawer 구현

--- presentation/src/main/java/com/example/presentation/view/HomeScreen.kt
+++ presentation/src/main/java/com/example/presentation/view/HomeScreen.kt
@@ -3,10 +3,12 @@ package com.example.presentation.view
 import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -15,10 +17,13 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.DrawerValue
 import androidx.compose.material.IconButton
+import androidx.compose.material.ModalDrawer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.rememberDrawerState
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -28,6 +33,7 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,6 +46,7 @@ import androidx.compose.ui.unit.dp
 import com.example.presentation.R
 import com.example.presentation.logOutButton
 import com.example.presentation.viewModel.MainViewModel
+import kotlinx.coroutines.launch
 import kotlin.math.log
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,49 +55,60 @@ fun HomeScreen(viewModel: MainViewModel) {
     val isLoggedIn by viewModel.isLoggedInState
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     val pagerState = rememberPagerState(pageCount = { 3 })
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+    val drawerState = rememberDrawerState(DrawerValue.Closed) // 메뉴의 초기 상태를 '닫힘'으로 설정
+    val scope = rememberCoroutineScope() // 코루틴을 통해 메뉴 열림/닫힘을 제어
+
+    ModalDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            DrawerContent ()
+        }
     ) {
-        HomeTopBar(scrollBehavior = scrollBehavior, isLoggedIn = isLoggedIn)
-        HorizontalPager(
-            state = pagerState,
-            pageSize = PageSize.Fill
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.sowoon_bg),
-                contentDescription = null,
-                modifier = Modifier.fillMaxWidth(),
-                contentScale = ContentScale.FillWidth
-            )
-        }
-        Row(
-            modifier = Modifier
-                .wrapContentHeight()
-                .fillMaxWidth()
-//                    .align(Alignment.Center)
-                .padding(top = 8.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            repeat(pagerState.pageCount) { iteration ->
-                val color = if (pagerState.currentPage == iteration) Color.Black else Color.Gray
-                Box(
-                    modifier = Modifier
-                        .padding(2.dp)
-                        .clip(CircleShape)
-                        .background(color)
-                        .size(12.dp)
+            HomeTopBar(scrollBehavior = scrollBehavior, isLoggedIn = isLoggedIn, onNavigationOnClick = { scope.launch { drawerState.open() }})
+            HorizontalPager(
+                state = pagerState,
+                pageSize = PageSize.Fill
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.sowoon_bg),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillWidth
                 )
-
             }
-        }
+            Row(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth()
+//                    .align(Alignment.Center)
+                    .padding(top = 8.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                repeat(pagerState.pageCount) { iteration ->
+                    val color = if (pagerState.currentPage == iteration) Color.Black else Color.Gray
+                    Box(
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .clip(CircleShape)
+                            .background(color)
+                            .size(12.dp)
+                    )
 
-        logOutButton(viewModel = viewModel)
+                }
+            }
+
+            logOutButton(viewModel = viewModel)
+        }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeTopBar(scrollBehavior: TopAppBarScrollBehavior, isLoggedIn: Boolean){
+fun HomeTopBar(scrollBehavior: TopAppBarScrollBehavior, isLoggedIn: Boolean, onNavigationOnClick: () -> Unit){
     val context = LocalContext.current
     CenterAlignedTopAppBar(
         title = { Text(text = "Sowoon", textAlign = TextAlign.Center) },
@@ -100,9 +118,10 @@ fun HomeTopBar(scrollBehavior: TopAppBarScrollBehavior, isLoggedIn: Boolean){
             navigationIconContentColor = Color.Black,
             actionIconContentColor = Color.Black
         ),
-        navigationIcon = { Column(modifier = Modifier.padding(start = 5.dp)) {
-            Icon(Icons.Filled.List, contentDescription = null, Modifier.size(30.dp))
-        } },
+        navigationIcon = { IconButton(onClick = { onNavigationOnClick() }) {
+                Icon(Icons.Filled.List, contentDescription = null, Modifier.size(30.dp))
+            }
+        },
         actions = {
             if(!isLoggedIn){
                 IconButton(
@@ -117,4 +136,17 @@ fun HomeTopBar(scrollBehavior: TopAppBarScrollBehavior, isLoggedIn: Boolean){
         },
         scrollBehavior = scrollBehavior
     )
+}
+
+@Composable
+fun DrawerContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(text = "Menu Item 1")
+        Text(text = "Menu Item 2")
+        Text(text = "Menu Item 3")
+    }
 }
