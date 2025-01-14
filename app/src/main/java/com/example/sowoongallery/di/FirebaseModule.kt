1af25099b24963f30f24da5fb245d4588bc4dacd
yunsuk990 <yunsuk990@naver.com>
2025-01-14 23:32:52 +0900

    Update FirebaseModule.kt

--- app/src/main/java/com/example/sowoongallery/di/FirebaseModule.kt
+++ app/src/main/java/com/example/sowoongallery/di/FirebaseModule.kt
@@ -1,8 +1,6 @@
 package com.example.sowoongallery.di
 
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
