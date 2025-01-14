61249364f98f5c59a8f23764961202f27c2e7469
yunsuk990 <yunsuk990@naver.com>
2025-01-14 23:32:57 +0900

    Create DataArtwork.kt

---
+++ data/src/main/java/com/example/data/model/DataArtwork.kt
@@ -0,0 +1,11 @@
+package com.example.data.model
+
+data class DataArtwork(
+    val name: String,
+    val category: String,
+    val madeIn: String,
+    val material: String,
+    val size: String,
+    val upload_at: String,
+    val url: String
+)
