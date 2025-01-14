a4cf8815b6e6ae2335e388112a8e2d9b12585b63
yunsuk990 <yunsuk990@naver.com>
2025-01-14 23:35:30 +0900

    Update MainActivity.kt

--- presentation/src/main/java/com/example/presentation/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/MainActivity.kt
@@ -1,37 +1,22 @@
 package com.example.presentation
 
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.IconButton
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -39,9 +24,11 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.presentation.model.Screen
+import com.example.presentation.view.BookMarkScreen
 import com.example.presentation.view.HomeScreen
+import com.example.presentation.view.LikedScreen
 import com.example.presentation.view.ProfileScreen
-import com.example.presentation.view.StartActivity
+import com.example.presentation.view.SettingScreen
 import com.example.presentation.view.ui.theme.SowoonTheme
 import com.example.presentation.viewModel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -52,6 +39,7 @@ class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             SowoonTheme {
                 MainScreen(viewModel = viewModel)
@@ -68,7 +56,7 @@ fun MainScreen(viewModel: MainViewModel){
     Scaffold(
         bottomBar = { BottomAppBar(navController) },
     ) { innerPadding ->
-        MyAppNavHost(navController = navController, modifier = Modifier.padding(innerPadding), viewModel )
+        MyAppNavHost(navController = navController, modifier = Modifier.padding(innerPadding).background(Color.White), viewModel )
     }
 
 
@@ -77,26 +65,29 @@ fun MainScreen(viewModel: MainViewModel){
 @Composable
 fun MyAppNavHost(navController: NavHostController, modifier: Modifier = Modifier, viewModel: MainViewModel){
     NavHost(navController = navController, startDestination = Screen.Home.route, modifier = modifier){
-        composable(Screen.Home.route) { HomeScreen(viewModel) }
-        composable(Screen.Profile.route) { ProfileScreen() }
+        composable(Screen.Home.route) { HomeScreen(viewModel, navController) }
+        composable(Screen.Profile.route) { ProfileScreen(viewModel) }
+        composable(Screen.Favorite.route) { LikedScreen(viewModel, navController)}
+        composable(Screen.BookMark.route) { BookMarkScreen(viewModel, navController) }
+        composable(Screen.Setting.route) { SettingScreen(viewModel, navController) }
     }
 }
 
 @Composable
 fun BottomAppBar(navController: NavHostController) {
-    var items = listOf(Screen.Home, Screen.Profile)
+    var items = listOf(Screen.Home, Screen.Profile, Screen.Setting)
     BottomNavigation(
         backgroundColor = Color.White
     ) {
         val backStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = backStackEntry?.destination?.route
 
-        items.forEach{ screen ->
+        items.forEach { screen ->
             BottomNavigationItem(
                 selected = currentRoute == screen.route,
                 onClick = {
-                    navController.navigate(screen.route){
-                        popUpTo(navController.graph.findStartDestination().id){
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
                             saveState = true
                         }
                         launchSingleTop = true
@@ -108,12 +99,5 @@ fun BottomAppBar(navController: NavHostController) {
                 //label = {Text(text =  screen.route )}
             )
         }
-    }
-}
-
-@Composable
-fun logOutButton(viewModel: MainViewModel){
-    OutlinedButton(onClick = { viewModel.logOut()} ){
-        Text(text = "로그아웃")
     }
 }
