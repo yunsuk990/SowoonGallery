8fe21ebccfd2bdc48a6a7509a47272dd7d8ce603
yunsuk990 <yunsuk990@naver.com>
2025-01-26 19:54:29 +0900

    Update bottomNavigation Icon

--- presentation/src/main/java/com/example/presentation/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/MainActivity.kt
@@ -6,16 +6,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.LocalRippleConfiguration
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Composition
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -31,7 +50,9 @@ import com.example.presentation.view.ProfileScreen
 import com.example.presentation.view.SettingScreen
 import com.example.presentation.view.ui.theme.SowoonTheme
 import com.example.presentation.viewModel.MainViewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.internal.wait
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -39,9 +60,14 @@ class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
+            val systemUiController = rememberSystemUiController()
+            systemUiController.setStatusBarColor(
+                color = Color.White,
+                darkIcons = !isSystemInDarkTheme()
+            )
             SowoonTheme {
+
                 MainScreen(viewModel = viewModel)
             }
         }
@@ -56,7 +82,9 @@ fun MainScreen(viewModel: MainViewModel){
     Scaffold(
         bottomBar = { BottomAppBar(navController) },
     ) { innerPadding ->
-        MyAppNavHost(navController = navController, modifier = Modifier.padding(innerPadding).background(Color.White), viewModel )
+        MyAppNavHost(navController = navController, modifier = Modifier
+            .padding(innerPadding)
+            .background(Color.White), viewModel )
     }
 
 
@@ -75,17 +103,19 @@ fun MyAppNavHost(navController: NavHostController, modifier: Modifier = Modifier
 
 @Composable
 fun BottomAppBar(navController: NavHostController) {
+    var onItemSelected by remember { mutableStateOf(0) }
     var items = listOf(Screen.Home, Screen.Profile, Screen.Setting)
+
     BottomNavigation(
         backgroundColor = Color.White
     ) {
         val backStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = backStackEntry?.destination?.route
 
-        items.forEach { screen ->
+        items.forEachIndexed{ index, screen ->
             BottomNavigationItem(
-                selected = currentRoute == screen.route,
+                selected = onItemSelected == index,
                 onClick = {
+                    onItemSelected = index
                     navController.navigate(screen.route) {
                         popUpTo(navController.graph.findStartDestination().id) {
                             saveState = true
@@ -95,8 +125,81 @@ fun BottomAppBar(navController: NavHostController) {
                     }
                 },
                 selectedContentColor = Color.Gray,
-                icon = { Icon(screen.icon, contentDescription = null) },
-                //label = {Text(text =  screen.route )}
+                unselectedContentColor = Color.Gray,
+                label = {
+                    Text(
+                        text = screen.title,
+                        fontSize = 12.sp,
+                        color = if(onItemSelected == index){
+                            Color.Black
+                        }else{
+                            Color.LightGray
+                        },
+                    )
+                },
+                icon = {
+                    Icon(
+                        painter = if(onItemSelected == index) painterResource(id = items[index].iconClicked) else painterResource(id = items[index].iconBorder),
+                        tint = if(onItemSelected == index) Color.Black else Color.LightGray,
+                        contentDescription = null,
+                        modifier = Modifier.size(35.dp).padding(1.dp)
+                    )
+                },
+                alwaysShowLabel = true,
+                modifier = Modifier.padding(top = 5.dp, bottom = 5.dp)
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun mainActivityTest(){
+    val navController = rememberNavController()
+    var onItemSelected by remember { mutableStateOf(0) }
+    var items = listOf(Screen.Home, Screen.Profile, Screen.Setting)
+    BottomNavigation(
+        backgroundColor = Color.White,
+        modifier = Modifier.wrapContentSize().padding(20.dp)
+    ) {
+        val backStackEntry by navController.currentBackStackEntryAsState()
+
+        items.forEachIndexed{ index, screen ->
+            BottomNavigationItem(
+                selected = onItemSelected == index,
+                onClick = {
+                    onItemSelected = index
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                selectedContentColor = Color.Gray,
+                unselectedContentColor = Color.Gray,
+                label = {
+                    Text(
+                        text = screen.title,
+                        fontSize = 10.sp,
+                        color = if(onItemSelected == index){
+                            Color.Black
+                        }else{
+                            Color.LightGray
+                        },
+                    )
+                },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.person_border),
+                        tint = if(onItemSelected == index) Color.Black else Color.LightGray,
+                        contentDescription = null,
+                        modifier = Modifier.size(32.dp).padding(1.dp)
+                    )
+                },
+                alwaysShowLabel = true,
+                modifier = Modifier.padding(top = 10.dp)
             )
         }
     }
--- presentation/src/main/java/com/example/presentation/model/Screen.kt
+++ presentation/src/main/java/com/example/presentation/model/Screen.kt
@@ -1,22 +1,12 @@
 package com.example.presentation.model
 
-import androidx.appcompat.content.res.AppCompatResources
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import com.example.presentation.R
 
-sealed class Screen(val route: String, val icon: ImageVector){
-    object Home : Screen("home", Icons.Filled.Home)
-    object Favorite : Screen("favorites", Icons.Filled.Favorite)
-    object BookMark: Screen("bookmark", Icons.Filled.ShoppingCart)
-    object Profile : Screen("profile", Icons.Filled.Person)
-    object Setting : Screen("setting", Icons.Filled.Settings)
+sealed class Screen(val route: String, val title: String, val iconClicked: Int, val iconBorder: Int){
+    object Home : Screen("home", "홈", R.drawable.home_filled, R.drawable.home_border)
+    object Favorite : Screen("favorites", "좋아요",R.drawable.home_border, R.drawable.home_filled)
+    object BookMark: Screen("bookmark", "북마크",R.drawable.home_border, R.drawable.home_filled)
+    object Profile : Screen("profile", "작품",R.drawable.art_filled, R.drawable.art_border)
+    object Setting : Screen("my", "MY",R.drawable.person_filled, R.drawable.person_border)
 }
