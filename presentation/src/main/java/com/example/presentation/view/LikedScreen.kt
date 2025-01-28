5de00bf1c5bc23aebc4ddca091c4713352d8010a
yunsuk990 <yunsuk990@naver.com>
2025-01-28 23:59:00 +0900

    Update LikedScreen.kt

--- presentation/src/main/java/com/example/presentation/view/LikedScreen.kt
+++ presentation/src/main/java/com/example/presentation/view/LikedScreen.kt
@@ -1,85 +1,73 @@
 package com.example.presentation.view
 
-import android.content.Context
-import android.content.Intent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.domain.model.DomainArtwork
+import com.example.presentation.R
+import com.example.presentation.utils.noRippleClickable
 import com.example.presentation.viewModel.MainViewModel
-import com.google.gson.Gson
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LikedScreen(viewModel: MainViewModel, navController: NavController) {
-    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     val likedItem by viewModel.artworkLikedLiveData.collectAsState()
-    val context = LocalContext.current
     LaunchedEffect(key1 = likedItem){
         viewModel.getLikedArtworksList()
     }
     Column {
-        LikedTopBar(scrollBehavior = scrollBehavior)
-        likedContent(likedItem, viewModel, context)
+        LikedTopBar(navController)
+        artworkGridLayout(artworkList = likedItem)
     }
 
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LikedTopBar(scrollBehavior: TopAppBarScrollBehavior) {
+fun LikedTopBar(navController: NavController) {
+    val interactionSource by remember { mutableStateOf(MutableInteractionSource()) }
     CenterAlignedTopAppBar(
-        title = { Text(text = "Favorites", textAlign = TextAlign.Center) },
+        title = { Text(text = "Favorites", textAlign = TextAlign.Center, style = MaterialTheme.typography.titleMedium) },
         colors = TopAppBarDefaults.smallTopAppBarColors(
             containerColor = Color.White,
             titleContentColor = Color.Black,
             navigationIconContentColor = Color.Black,
             actionIconContentColor = Color.Black
         ),
-        navigationIcon = {},
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    navController.popBackStack()
+                },
+                modifier = Modifier.noRippleClickable {
+                    navController.popBackStack()
+                }
+            ) {
+                Icon(painter = painterResource(id = R.drawable.back), contentDescription = "뒤로가기")
+            }
+        },
         actions = {},
-        scrollBehavior = scrollBehavior,
     )
     Divider(thickness = 0.5.dp, color = Color.LightGray)
-}
-@Composable
-fun likedContent(likedItem: List<DomainArtwork>?, viewModel: MainViewModel,  context: Context) {
-    Column {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            contentPadding = PaddingValues(8.dp),
-            horizontalArrangement =  Arrangement.spacedBy(10.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            content = {
-                likedItem?.let { item ->
-                items(likedItem.size){
-                    artworkCard(artwork = likedItem!![it], viewModel){
-                        context.startActivity(Intent(context, ArtworkActivity::class.java).apply {
-                            putExtra("artwork", Gson().toJson(likedItem!![it]))
-                        })
-                    }
-                }
-            }
-        })
-    }
 }
