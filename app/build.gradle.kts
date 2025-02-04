77ee36b4cec1c70ac8bb5b2f138786537cc792a3
yunsuk990 <yunsuk990@naver.com>
2025-02-04 23:55:57 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,6 @@ dependencies {
     //hilt
     implementation("com.google.dagger:hilt-android:2.52")
     kapt("com.google.dagger:hilt-android-compiler:2.52")
+
+    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
 }
