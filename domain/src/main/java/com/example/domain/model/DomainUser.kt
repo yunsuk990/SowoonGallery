dfb32db942ee8f25c011f3b0550e5e391b4fc086
yunsuk990 <yunsuk990@naver.com>
2025-02-07 23:47:44 +0900

    Update DomainUser.kt

--- domain/src/main/java/com/example/domain/model/DomainUser.kt
+++ domain/src/main/java/com/example/domain/model/DomainUser.kt
@@ -5,8 +5,9 @@ data class DomainUser(
     var name: String = "",
     var age: Int = 0,
     var profileImage: String = "",
-    var favoriteArtworks: Map<String, String> = emptyMap(),
-    var likedArtworks: Map<String, String> = emptyMap(),
-){
-
-}
+    var mode: Int = 0,  //Artist:1 또는 User:0
+    var review: String = "",
+    var artworksUid: Map<String, Boolean> = emptyMap(),
+    var favoriteArtworks: Map<String, Boolean> = emptyMap(),
+    var likedArtworks: Map<String, Boolean> = emptyMap(),
+)
