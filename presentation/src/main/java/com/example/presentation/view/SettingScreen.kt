8123c92dddadc4ab1d731bf78d45bf5d56fe417c
yunsuk990 <yunsuk990@naver.com>
2025-02-07 23:51:13 +0900

    Update SettingScreen.kt

--- presentation/src/main/java/com/example/presentation/view/SettingScreen.kt
+++ presentation/src/main/java/com/example/presentation/view/SettingScreen.kt
@@ -1,243 +1,211 @@
 package com.example.presentation.view
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
+import android.content.Intent
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.TabRow
-import androidx.compose.material.TabRowDefaults
-import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Tab
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import coil3.compose.AsyncImage
+import com.example.domain.model.DomainUser
 import com.example.presentation.R
 import com.example.presentation.model.Screen
+import com.example.presentation.utils.LogOutToastMessage
+import com.example.presentation.utils.LoginToastMessage
 import com.example.presentation.viewModel.MainViewModel
-
-
-@Composable
-fun SettingScreen(
-    viewModel: MainViewModel,
-    navController: NavHostController,
-){
-    var openProfileDialog by remember { mutableStateOf(false) }
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        profileUser(navController, viewModel) { openProfileDialog = true }
-        Spacer(modifier = Modifier.height(30.dp))
-        profileMenu()
-    }
-    if(openProfileDialog){
-        ProfileEditDialog(viewModel) { openProfileDialog = false }
-    }
-
-}
+import com.google.gson.Gson
 
 @Composable
-fun profileUser(
-    navController: NavHostController,
-    viewModel: MainViewModel,
-    profileOnClick: () -> Unit,
-) {
+fun  SettingScreen(viewModel: MainViewModel, navController: NavHostController){
 
     val userInfo by viewModel.userInfoStateFlow.collectAsState()
+    val context = LocalContext.current
+    var requestLogin by remember { mutableStateOf(false) }
+    var requestLogOut by remember { mutableStateOf(false) }
 
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 30.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(
+    Scaffold(
+        topBar = { SettingTopAppBar(navController = navController) },
+    ) { innerPadding ->
+        SettingRoot(
             modifier = Modifier
-                .clip(RoundedCornerShape(100.dp))
-                .size(100.dp)
-                .background(color = colorResource(id = R.color.lightgray))
-                .clickable {
-//                    navController.navigate(Screen.ProfileEdit.route) {
-//                        launchSingleTop = true
-//                    }
-                    profileOnClick()
-                },
-            contentAlignment = Alignment.Center,
-        ){
-            if(userInfo.profileImage != null){
-                AsyncImage(model = userInfo.profileImage, contentDescription = null, contentScale = ContentScale.Crop)
-            }else{
-                Icon(painter = painterResource(id = R.drawable.profile), contentDescription = null, modifier = Modifier.size(24.dp))
-            }
-        }
-        Text(text = userInfo.name, letterSpacing = 1.sp ,fontSize = 20.sp, color = Color.Black, modifier = Modifier.padding(top = 25.dp))
-        Row(
-            modifier = Modifier.padding(top = 25.dp, start = 20.dp, end = 20.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            userSection(
-                modifier = Modifier.weight(1f),
-                "Liked",
-                R.drawable.heart_border,
-                userInfo.likedArtworks.size
-            ) {
-                navController.navigate(Screen.Favorite.route) {
-                    launchSingleTop = true
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(Color.White)
+                .verticalScroll(rememberScrollState()),
+            userInfo = userInfo,
+            requestLogin = { requestLogin = true },
+            actionBtn = {
+                if(userInfo.uid.isEmpty()){
+                    actionButton(text = "로그인", onClick = {
+                        context.startActivity(Intent(context, StartActivity::class.java))
+                    })
+                }else{
+                    actionButton(text = "로그아웃", onClick = {
+                        requestLogOut = true
+                    })
                 }
             }
-            userSection(
-                modifier = Modifier.weight(1f),
-                "BookMark",
-                R.drawable.bookmark_border,
-                userInfo.favoriteArtworks.size
-            ){
-                navController.navigate(Screen.BookMark.route) {
-                    launchSingleTop = true
+        )
+
+        if(requestLogin){
+            LoginToastMessage(
+                dismissOnClick = { requestLogin = false },
+                confirmOnClick = {
+                    context.startActivity(Intent(context, StartActivity::class.java))
+                    requestLogin = false
+                }
+            )
+        }
+
+        if(requestLogOut){
+            LogOutToastMessage(
+                dismissOnClick = { requestLogOut = false },
+                confirmOnClick = {
+                    viewModel.logOut()
+                    navController.navigate(Screen.Home.route){
+                        launchSingleTop = true
+                    }
+                    requestLogOut = false
+                }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun SettingRoot(
+    modifier: Modifier = Modifier,
+    userInfo: DomainUser,
+    actionBtn: @Composable () -> Unit,
+    requestLogin: () -> Unit
+) {
+    val context = LocalContext.current
+    Column(modifier = modifier) {
+        Text(text = "Settings", color = Color.Black, fontSize = 16.sp, modifier = Modifier.padding(start = 15.dp, top = 20.dp))
+        menuItem(
+            title = "프로필 수정",
+            onClick = {
+                if(userInfo.uid.isEmpty()){
+                    requestLogin()
+                }else{
+                    context.startActivity(Intent(context, ProfileEditActivity::class.java).putExtra("userInfo", Gson().toJson(userInfo)))
                 }
             }
-        }
+        )
+        menuItem("계정 관리"){}
+        menuItem("앱 관리"){}
+        menuItem("공지사항"){}
+        menuItem("문의하기"){}
+        menuItem("이벤트"){}
+        Spacer(modifier = Modifier.weight(1f))
+        actionBtn()
     }
 }
 
 @Composable
-fun userSection(modifier: Modifier, title: String, icon: Int, size: Int, onClick: () -> Unit){
-    val interactionSource by remember { mutableStateOf(MutableInteractionSource()) }
-    Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-            .wrapContentSize()
-            .clickable(indication = null, interactionSource = interactionSource) {
-                onClick()
-            }
-    ){
-        Text(text = size.toString(), fontSize = 16.sp, color = Color.Black, fontWeight = FontWeight.Bold)
-        Spacer(modifier = Modifier.height(8.dp))
+fun menuItem(title: String, onClick: () -> Unit){
+
+    Column(
+        modifier = Modifier.clickable {
+            onClick()
+        }
+    ) {
         Row(
-            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp, top = 25.dp, bottom = 25.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(painter = painterResource(id = icon), contentDescription = "좋아요 작품", modifier = Modifier.size(20.dp))
-            Spacer(modifier = Modifier.width(8.dp))
+            Icon(painter = painterResource(id = R.drawable.alert), contentDescription = null)
+            Spacer(modifier = Modifier.width(15.dp))
             Text(text = title, fontSize = 16.sp, color = Color.Black)
         }
+        Divider(thickness = 0.5.dp, color = colorResource(id = R.color.lightgray))
     }
 }
+
+@Composable
+fun actionButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    OutlinedButton(
+        modifier = Modifier
+            .padding(start = 15.dp, end = 15.dp, bottom = 20.dp)
+            .fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.Black
+        ),
+        shape = RoundedCornerShape(30.dp),
+        contentPadding = PaddingValues(vertical = 15.dp),
+        onClick = {
+            onClick()
+        },
+    ) { Text(text, fontSize = 16.sp, color = Color.White) }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingTopAppBar(navController: NavController){
+    Column {
+        CenterAlignedTopAppBar(
+            title = { Text( text = "설정", style = MaterialTheme.typography.titleMedium) },
+            navigationIcon = {
+                IconButton(onClick = {
+                    navController.popBackStack()
+                }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.back),
+                        contentDescription = "뒤로가기"
+                    )
+                }
+            },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = Color.White,
+                titleContentColor = Color.Black,
+                navigationIconContentColor = Color.Black,
+                actionIconContentColor = Color.Black
+            ),
+            actions = {},
+        )
+        Divider(thickness = 0.5.dp, color = Color.LightGray)
+    }
+}
+
 
 
 @Preview(showSystemUi = true)
 @Composable
-fun SettingScreenTest(){
-    val navController = rememberNavController()
-    Surface(modifier = Modifier
-        .background(Color.White)
-        .fillMaxSize()) {
-        Column(modifier = Modifier.background(Color.White)) {
-        }
-    }
-}
-
-@Composable
-fun profileMenu(){
-    var selectedIndex by remember { mutableStateOf(0) }
-    val tabTitles = listOf("설정", "구매한 작품")
-    Column {
-        TabRow(
-            selectedTabIndex = selectedIndex,
-            backgroundColor = Color.White,
-            indicator = { tabPositions ->
-                TabRowDefaults.Indicator(
-                    Modifier.tabIndicatorOffset(tabPositions[selectedIndex]),
-                    color = Color.Black,
-                    height = 1.dp
-                )
-            }
-        ) {
-
-            tabTitles.forEachIndexed { index, title ->
-                Tab(
-                    selected = selectedIndex == index,
-                    text = {
-                        Text(
-                            text = title,
-                            fontSize = 16.sp,
-                            color = if(selectedIndex == index){
-                                Color.Black
-                            }else{
-                                Color.LightGray
-                            }
-                        ) },
-                    onClick = { selectedIndex = index}
-                )
-            }
-        }
-        when(selectedIndex){
-            0 -> settingScreen()
-            1 -> differentArtworks()
-        }
-    }
-
-}
-
-@Composable
-fun settingScreen() {
-    Column(modifier = Modifier.padding(start = 20.dp, top = 10.dp, end = 20.dp)) {
-        menuItem()
-        menuItem()
-        menuItem()
-        menuItem()
-        menuItem()
-
-    }
-}
-
-@Composable
-fun menuItem(){
-
-    Column(
-        modifier = Modifier
-    ) {
-        Row(
-            modifier = Modifier.padding(top = 20.dp, bottom = 20.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(painter = painterResource(id = R.drawable.alert), contentDescription = "앱 관리")
-            Spacer(modifier = Modifier.width(15.dp))
-            Text(text = "앱 관리", fontSize = 16.sp, color = Color.Black)
-        }
-        Divider(thickness = 0.5.dp, color = colorResource(id = R.color.lightgray))
+fun TestSettingScreen(){
+    var navController = rememberNavController()
+    Scaffold(
+        topBar = { SettingTopAppBar(navController)},
+    ) { innerpadding ->
+        SettingRoot(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerpadding)
+                .background(Color.White)
+                .verticalScroll(rememberScrollState()),
+            userInfo = DomainUser(),
+            actionBtn = {
+                actionButton ("로그인") { }
+            },
+            requestLogin = {}
+        )
     }
 }
