e358abb5b23cb89010efb054b6e82d3392a4ab05
yunsuk990 <yunsuk990@naver.com>
2025-02-07 23:47:39 +0900

    Update DomainChatRoomWithUser.kt

--- domain/src/main/java/com/example/domain/model/DomainChatRoomWithUser.kt
+++ domain/src/main/java/com/example/domain/model/DomainChatRoomWithUser.kt
@@ -2,5 +2,6 @@ package com.example.domain.model
 
 data class DomainChatRoomWithUser(
     var destUser: DomainUser,
-    var chatRoom: DomainChatRoom
+    var chatRoom: DomainChatRoom,
+    var artwork: DomainArtwork
 )
