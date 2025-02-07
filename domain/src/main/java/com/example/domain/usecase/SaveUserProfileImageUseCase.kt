6d0895af414ed79dc126d2e2c5457df686ca5885
yunsuk990 <yunsuk990@naver.com>
2025-02-07 23:48:58 +0900

    Update SaveUserProfileImageUseCase.kt

--- domain/src/main/java/com/example/domain/usecase/SaveUserProfileImageUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/SaveUserProfileImageUseCase.kt
@@ -2,11 +2,12 @@ package com.example.domain.usecase
 
 import android.net.Uri
 import com.example.domain.model.DomainUser
+import com.example.domain.repository.AuthRepository
 import com.example.domain.repository.FirebaseRepository
 import javax.inject.Inject
 
 class SaveUserProfileImageUseCase @Inject constructor(
-    private val firebaseRepository: FirebaseRepository
+    private val authRepository: AuthRepository
 ) {
-    suspend fun execute(uid: String, uri: Uri?, currentUser: DomainUser, name: String, age: Int) = firebaseRepository.uploadProfileImage(uid,uri, currentUser, name, age)
+    suspend fun execute(uri: Uri?, currentUser: DomainUser, updateUser: DomainUser) = authRepository.updateProfileInfo(uri, currentUser, updateUser)
 }
