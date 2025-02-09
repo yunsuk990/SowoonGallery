25c604d3166f67f387a0f62df0cf467f21a91f22
yunsuk990 <yunsuk990@naver.com>
2025-02-09 23:52:33 +0900

    Update StartActivity.kt

--- presentation/src/main/java/com/example/presentation/view/StartActivity.kt
+++ presentation/src/main/java/com/example/presentation/view/StartActivity.kt
@@ -7,16 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
@@ -41,6 +32,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.presentation.R
+import com.example.presentation.utils.noRippleClickable
 import com.example.presentation.view.ui.theme.SowoonTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
@@ -70,43 +62,47 @@ class StartActivity : ComponentActivity() {
 }
 
 @Composable
-@Preview(showBackground = true, showSystemUi = true)
 private fun MainScreen(){
     val context = LocalContext.current
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        val logoPainter = painterResource(R.drawable.logo)
-        Image(
-            painter = logoPainter,
-            contentDescription = "Background",
-            modifier = Modifier.size(180.dp),
-            contentScale = ContentScale.Fit
-        )
-    }
-
-    Column (
-        verticalArrangement = Arrangement.Bottom,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-
-        filledTonalButtonExample {
-            context.startActivity(Intent(context, LoginActivity::class.java))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val logoPainter = painterResource(R.drawable.logo)
+            Image(
+                painter = logoPainter,
+                contentDescription = "Background",
+                modifier = Modifier.size(180.dp),
+                contentScale = ContentScale.Fit
+            )
         }
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically
+        Column (
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.align(Alignment.BottomCenter)
         ){
-            Text(text = "이미 계정이 있나요?", color = Color.Gray, fontSize = 14.sp)
 
-            TextButton(onClick = {
+            filledTonalButtonExample {
                 context.startActivity(Intent(context, LoginActivity::class.java))
-            }) {
-                Text(text = "로그인", color = Color.Black)
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(vertical = 15.dp)
+            ){
+                Text(text = "이미 계정이 있나요?", color = Color.Gray, fontSize = 14.sp)
+                Text(
+                    text = "로그인", color = Color.Black, fontSize = 14.sp,
+                    modifier = Modifier.padding(start = 8.dp).noRippleClickable {
+                        context.startActivity(Intent(context, LoginActivity::class.java))
+                    })
             }
         }
     }
@@ -125,5 +121,11 @@ fun filledTonalButtonExample(onClick: () -> Unit) {
     ) {
         Text("시작하기", fontSize = 16.sp, color = Color.Black)
     }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun StartActivityTest(){
+    MainScreen()
 }
 
