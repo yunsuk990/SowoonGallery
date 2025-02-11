c2b12a5bb5e050023bf557f8bd13f1f35fd7a678
yunsuk990 <yunsuk990@naver.com>
2025-02-11 23:49:19 +0900

    Update build.gradle.kts

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -57,27 +57,29 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.3.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+    //Gson
     implementation("com.google.code.gson:gson:2.11.0")
+
     implementation(project(mapOf("path" to ":domain")))
+
+    //Coil
     implementation("io.coil-kt.coil3:coil-compose:3.0.0-rc02")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.0-rc02")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.36.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
@@ -87,6 +89,7 @@ dependencies {
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
     implementation("androidx.navigation:navigation-compose:2.8.0")
+
     implementation("androidx.compose.material:material:1.7.5")
     implementation("androidx.compose.foundation:foundation:1.7.4")
 
