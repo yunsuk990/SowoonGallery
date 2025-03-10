71a13f5a841756c6ca8f73c02eded735440c4eda
yunsuk990 <yunsuk990@naver.com>
2025-03-10 23:26:12 +0900

    채팅방 접속 여부

---
+++ domain/src/main/java/com/example/domain/usecase/chatUseCase/SetUserPresenceUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.domain.usecase.chatUseCase
+
+import com.example.domain.repository.FirebaseRepository
+import javax.inject.Inject
+
+class SetUserPresenceUseCase @Inject constructor(
+    private val firebaseRepository: FirebaseRepository
+){
+    suspend fun leaveChatRoom(roomId: String, uid: String) = firebaseRepository.leaveChatRoom(roomId, uid)
+    suspend fun enterChatRoom(roomId: String, uid: String) = firebaseRepository.enterChatRoom(roomId, uid)
+
+}
