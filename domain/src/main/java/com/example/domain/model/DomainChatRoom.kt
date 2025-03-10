11faa8d468f4f1c642abf94e853d3bc186018323
yunsuk990 <yunsuk990@naver.com>
2025-03-10 23:26:50 +0900

    Update DomainChatRoom.kt

--- domain/src/main/java/com/example/domain/model/DomainChatRoom.kt
+++ domain/src/main/java/com/example/domain/model/DomainChatRoom.kt
@@ -3,8 +3,9 @@ package com.example.domain.model
 data class DomainChatRoom(
     var roomId: String = "",
     var artworkId: String = "",
-    var users: Map<String, Boolean> = HashMap(),
+    var users: Map<String, Boolean> = HashMap(),//채팅방 찾기 위한 용도
+    var status: Map<String, Boolean> = HashMap(),//uid : 접속여부,
     var lastMessage: DomainMessage = DomainMessage(),
     var unreadMessages: Map<String, Int> = HashMap(),
-    var createdAt: String = ""
+    var createdAt: String = "",
 )
