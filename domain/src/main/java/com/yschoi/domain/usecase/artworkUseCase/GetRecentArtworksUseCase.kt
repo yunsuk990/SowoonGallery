58685f95c091606d2bf79c85aef311fc470d4598
yunsuk990 <yunsuk990@naver.com>
2025-04-06 23:28:33 +0900

    Update GetRecentArtworksUseCase.kt

--- domain/src/main/java/com/yschoi/domain/usecase/artworkUseCase/GetRecentArtworksUseCase.kt
+++ domain/src/main/java/com/yschoi/domain/usecase/artworkUseCase/GetRecentArtworksUseCase.kt
@@ -2,11 +2,14 @@ package com.yschoi.domain.usecase.artworkUseCase
 
 import com.yschoi.domain.model.DomainArtwork
 import com.yschoi.domain.repository.ArtworkRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
 import javax.inject.Inject
 
 class GetRecentArtworksUseCase @Inject constructor(
     private val artworkRepository: ArtworkRepository
 ) {
-    suspend fun execute(limit: Int): List<DomainArtwork> = artworkRepository.getRecentArtworks(limit)
-
+    suspend fun execute(
+        limit: Int
+    ): Flow<List<DomainArtwork>> = artworkRepository.getRecentArtworks(limit)
 }
